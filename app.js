05e2d4ad0c861c03438a35d8b9d4a73f9d7ef785
Wencly <78921411+Wencly@users.noreply.github.com>
2021-10-20 00:20:55 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -24,6 +24,6 @@ app.use(json());
 app.use("/api/books", books);
 
 //listen for request on port 3000, and as a callback function have the port listened on logged
-app.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+app.listen(port, () => {
+  console.log(`Server running at `);
 });
